444234a45eefb25d16e2142af5a4afd54b4ba3bf
alextuturescu <84986920+alextuturescu@users.noreply.github.com>
2022-01-07 20:17:02 +0200

    Add files via upload

---
+++ src/client.ts
@@ -0,0 +1,107 @@
+import { feeValue, heightDelta, minBoxValue, unitsInOneErgo } from './constants';
+import { Explorer } from './explorer';
+import { Address } from './models/address';
+import { ErgoBox } from './models/ergoBox';
+import { Transaction } from './models/transaction';
+import { Serializer } from './serializer';
+
+declare const console;
+
+export class Client {
+  public explorer: Explorer;
+
+  constructor(explorerUri = 'https://new-explorer.ergoplatform.com') {
+    this.explorer = new Explorer(explorerUri);
+  }
+
+  /**
+   * Transfer ERG or token, specifying the amount in nanoErgs or minimal token units.
+   *
+   * @param sk - secret key
+   * @param recipient - address ot the transaction recipient
+   * @param amountInt - amount to transfer in minimal units. Should be integer.
+   * @param tokenId - id of a token to send. 'ERG' if  going to send ERG
+   */
+  public async transferInt(sk: string, recipient: string, amountInt: number, tokenId: string = 'ERG') {
+    if (tokenId === 'ERG') {
+      return this.ergTransfer(recipient, amountInt, sk);
+    } else {
+      return this.tokenTransfer(recipient, tokenId, amountInt, sk);
+    }
+  }
+
+  /**
+   * Transfer ERG or token.
+   * Sent amount may be slightly different from the specified one due to JS float number representation.
+   * For exact transfer us transferInt method.
+   *
+   * @param sk - secret key
+   * @param recipient - address ot the transaction recipient
+   * @param amount - amount to transfer
+   * @param tokenId - id of a token to send. 'ERG' if  going to send ERG
+   */
+  public async transfer(sk: string, recipient: string, amount: number, tokenId: string = 'ERG') {
+    let factor = unitsInOneErgo;
+    if (tokenId !== 'ERG') {
+      factor = await this.tokenDecimalsFactor(tokenId);
+    }
+    return this.transferInt(sk, recipient, amount * factor, tokenId);
+  }
+
+  public async tokenIssue(sk: string, name: string, amount: number, decimals: number, description: string) {
+    const amountInt = amount * Math.pow(10, decimals);
+    const height = await this.explorer.getCurrentHeight();
+    const sender: Address = Address.fromSk(sk);
+    const myBoxes = await this.explorer.getUnspentOutputs(sender);
+    const basePayloadOuts = [new ErgoBox('', feeValue, height - heightDelta, sender)];
+    const boxesToSpend = ErgoBox.getSolvingBoxes(myBoxes, basePayloadOuts);
+    const token = {
+      amount: amountInt,
+      tokenId: boxesToSpend[0].id,
+    };
+    // Reminder: serializedByteArrayInRegister = '\x0e' + intToVlq(bytearray.length) + byteArray
+    const registers = ErgoBox.encodeRegisters({
+      R4: name,
+      R5: description,
+      R6: decimals,
+    });
+    const payloadOutsWithTokens = [new ErgoBox('', minBoxValue, height - heightDelta, sender, [token], registers)];
+    const unsignedTx = Transaction.fromOutputs(boxesToSpend, payloadOutsWithTokens);
+    const signedTx = unsignedTx.sign(sk);
+    return await this.explorer.broadcastTx(signedTx);
+  }
+
+  private async ergTransfer(recipient: string, amountInt: number, sk: string) {
+    const height = await this.explorer.getCurrentHeight();
+    const myBoxes = await this.explorer.getUnspentOutputs(Address.fromSk(sk));
+    const payloadOuts = [new ErgoBox('', amountInt, height - heightDelta, new Address(recipient))];
+    const boxesToSpend = ErgoBox.getSolvingBoxes(myBoxes, payloadOuts);
+    const unsignedTx = Transaction.fromOutputs(boxesToSpend, payloadOuts);
+    const signedTx = unsignedTx.sign(sk);
+    return await this.explorer.broadcastTx(signedTx);
+  }
+
+  private async tokenTransfer(recipient: string, tokenId: string, amountInt: number, sk: string) {
+    const height = await this.explorer.getCurrentHeight();
+    const tokens = [
+      {
+        amount: amountInt,
+        tokenId,
+      },
+    ];
+    const payloadOuts = [new ErgoBox('', minBoxValue, height - heightDelta, new Address(recipient), tokens)];
+    const myBoxes = await this.explorer.getUnspentOutputs(Address.fromSk(sk));
+    const boxesToSpend = ErgoBox.getSolvingBoxes(myBoxes, payloadOuts);
+    const unsignedTx = Transaction.fromOutputs(boxesToSpend, payloadOuts);
+    const signedTx = unsignedTx.sign(sk);
+    return await this.explorer.broadcastTx(signedTx);
+  }
+
+  private async tokenDecimalsFactor(tokenId: string) {
+    const tokenInfo = await this.explorer.getTokenInfo(tokenId);
+    const r6 = 'R6';
+    const R6: string = tokenInfo.additionalRegisters[r6];
+    const decimals = Number(Serializer.stringFromHex(R6.slice(4, R6.length)));
+    return Math.pow(10, decimals);
+  }
+}
---
+++ src/serializer.ts
@@ -0,0 +1,117 @@
+import * as Enc from '@root/encoding/hex';
+import * as flat from 'array.prototype.flat';
+import { ErgoBox } from './models/ergoBox';
+import { Input } from './models/input';
+import { Transaction } from './models/transaction';
+
+declare const Buffer;
+declare const Object;
+declare const Number;
+
+export class Serializer {
+  public static outputToBytes(out: ErgoBox, tokenIds) {
+    let res = this.intToVlq(out.value);
+    res = Buffer.concat([res, Buffer.from(out.ergoTree, 'hex')]);
+    res = Buffer.concat([res, this.intToVlq(out.creationHeight)]);
+
+    res = Buffer.concat([res, this.intToVlq(out.assets.length)]);
+    for (const asset of out.assets) {
+      const t = asset.tokenId;
+      const n = tokenIds.indexOf(t);
+      res = Buffer.concat([res, this.intToVlq(n)]);
+      res = Buffer.concat([res, this.intToVlq(asset.amount)]);
+    }
+    const k = Object.keys(out.additionalRegisters).length;
+    res = Buffer.concat([res, this.intToVlq(k)]);
+    for (let i = 4; i < k + 4; i += 1) {
+      res = Buffer.concat([res, Buffer.from(out.additionalRegisters['R' + i], 'hex')]);
+    }
+    return res;
+  }
+
+  public static inputToBytes(input: Input) {
+    let res = Buffer.from(input.boxId, 'hex');
+    const sp = input.spendingProof;
+    res = Buffer.concat([res, this.intToVlq(sp.proofBytes.length >> 1)]);
+    res = Buffer.concat([res, Buffer.from(sp.proofBytes, 'hex')]);
+    res = Buffer.concat([res, this.intToVlq(Object.keys(sp.extension).length)]);
+
+    Object.keys(sp.extension).forEach(k => {
+      res += this.intToVlq(Number(k));
+      res += this.valueSerialize(sp.extension[k]);
+    });
+
+    return res;
+  }
+
+  public static transactionToBytes(tx: Transaction) {
+    let res = this.intToVlq(tx.inputs.length);
+
+    Object.values(tx.inputs).forEach(input => {
+      res = Buffer.concat([res, this.inputToBytes(input)]);
+    });
+
+    res = Buffer.concat([res, this.intToVlq(tx.dataInputs.length)]);
+
+    tx.dataInputs.forEach(i => {
+      res = Buffer.concat([res, Buffer.from(i.boxId, 'hex')]);
+    });
+
+    const distinctIds = this.distinctTokenList(tx.outputs);
+
+    res = Buffer.concat([res, this.intToVlq(distinctIds.length)]);
+
+    Object.values(distinctIds).forEach(v => {
+      res = Buffer.concat([res, Buffer.from(v, 'hex')]);
+    });
+
+    res = Buffer.concat([res, this.intToVlq(tx.outputs.length)]);
+    Object.values(tx.outputs).forEach(v => {
+      res = Buffer.concat([res, this.outputToBytes(v, distinctIds)]);
+    });
+
+    return res;
+  }
+
+  public static intToVlq(num: number): string {
+    let x = num;
+    let res = Buffer.from([]);
+    let r;
+    while (parseInt(String(x / 2 ** 7), 10)) {
+      r = x & 0x7f;
+      x = parseInt(String(x / 2 ** 7), 10);
+      res = Buffer.concat([res, Buffer.from([r | 0x80], null, 1)]);
+    }
+    r = x & 0x7f;
+    res = Buffer.concat([res, Buffer.from([r], null, 1)]);
+    return res;
+  }
+
+  public static stringToHex(str: string) {
+    return Enc.strToHex(str);
+  }
+
+  public static stringFromHex(str: string) {
+    return Enc.hexToStr(str);
+  }
+
+  // todo (?) merge with ErgoBox.extractAssets()
+  protected static distinctTokenList(outputs: ErgoBox[]): string[] {
+    // todo use flatMap after switching to es2019
+    const flatTokenList = flat(outputs.map(output => output.assets.map(asset => asset.tokenId)));
+    const seenTokens = new Set();
+    const res = [];
+    for (const currId of flatTokenList) {
+      if (!seenTokens.has(currId)) {
+        res.push(currId);
+        seenTokens.add(currId);
+      }
+    }
+    return res;
+  }
+
+  // todo ?
+  protected static valueSerialize(v: string) {
+    return Buffer.from([]);
+  }
+}
